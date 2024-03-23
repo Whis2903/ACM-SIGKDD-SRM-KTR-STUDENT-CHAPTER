ae04e32fdc34ef51b6edafa6a934d6ce2e55dd57
ADITYA VERMA <adityav1304@gmail.com>
2024-03-23 13:40:06 +0530

    a

--- src/components/settingTabs/index.js
+++
@@ -1,5 +0,0 @@
-export {default as ProfileTab } from './profileTab'
-export {default as TeamTab } from './teamTab'
-export {default as EventTab } from './eventTab'
-
-export {default as ManageEventTab } from './manageEventTab'
